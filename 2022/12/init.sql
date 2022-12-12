76e58fc123aba8c0fb8270517324a3cc12647395
Per Scheffer <per.scheffer@nordicstation.com>
2022-12-12 13:57:08 +0100

    Day 12

---
+++ 2022/12/init.sql
@@ -0,0 +1,50 @@
+USE tempdb;
+
+--------------------------------------------------------------------------------
+--  READ FILE input.txt INTO TABLE Input
+--------------------------------------------------------------------------------
+DROP TABLE IF EXISTS dbo.Input;
+
+CREATE TABLE dbo.Input
+(
+    RowNumber int       IDENTITY(1, 1) NOT NULL,
+    Data      char(136) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
+    CONSTRAINT PK_Input PRIMARY KEY CLUSTERED (RowNumber)
+);
+GO
+
+CREATE OR ALTER VIEW dbo.vInput
+AS
+SELECT Data
+  FROM dbo.Input;
+GO
+
+BULK INSERT dbo.vInput
+FROM 'X:\12\input.txt'
+WITH (TABLOCK);
+
+DROP VIEW dbo.vInput;
+
+--------------------------------------------------------------------------------
+--  VIEW INPUT DATA
+--------------------------------------------------------------------------------
+SELECT *
+  FROM dbo.Input;
+
+WITH cte
+  AS (SELECT MAX(LEN(Data))                                           AS MaxLength,
+             2 * MAX(LEN(Data))                                       AS DoubleMaxLength,
+             POWER(10, CAST(FLOOR(LOG10(2 * MAX(LEN(Data)))) AS int)) AS Magnitude
+        FROM dbo.Input)
+SELECT c.MaxLength,
+       CASE
+            WHEN 1.0 * c.DoubleMaxLength / c.Magnitude <= 1 THEN
+                c.Magnitude
+            WHEN 1.0 * c.DoubleMaxLength / c.Magnitude <= 2 THEN
+                2 * c.Magnitude
+            WHEN 1.0 * c.DoubleMaxLength / c.Magnitude <= 5 THEN
+                5 * c.Magnitude
+            ELSE
+                10 * c.Magnitude
+       END AS DataSize
+  FROM cte AS c;
---
+++ 2022/12/part2.sql
@@ -0,0 +1,95 @@
+USE tempdb;
+
+DROP TABLE IF EXISTS dbo.ValidStep;
+DROP TABLE IF EXISTS dbo.Square;
+
+CREATE TABLE dbo.Square
+(
+    SquareNumber int NOT NULL,
+    Row          int NOT NULL,
+    Col          int NOT NULL,
+    Elevation    int NOT NULL,
+    IsStart      bit NOT NULL,
+    IsEnd        bit NOT NULL,
+    CONSTRAINT PK_Square PRIMARY KEY (SquareNumber)
+) AS NODE;
+GO
+
+CREATE TABLE dbo.ValidStep AS EDGE;
+
+CREATE UNIQUE INDEX IX_ValidStep
+ON dbo.ValidStep (
+    $from_id,
+    $to_id);
+
+WITH letter
+  AS (SELECT       CAST(ROW_NUMBER() OVER (ORDER BY i.RowNumber,
+                                                    gs.value) AS int) AS SquareNumber,
+                   i.RowNumber                                        AS Row,
+                   gs.value                                           AS Col,
+                   SUBSTRING(i.Data, gs.value, 1)                     AS Letter
+        FROM       dbo.Input                       AS i
+       CROSS APPLY GENERATE_SERIES(1, LEN(i.Data)) AS gs ),
+     square
+  AS (SELECT l.SquareNumber,
+             l.Row,
+             l.Col,
+             ASCII(CASE l.Letter
+                        WHEN 'S' THEN
+                            'a'
+                        WHEN 'E' THEN
+                            'z'
+                        ELSE
+                            l.Letter
+                   END) - 97 AS Elevation,
+             CASE
+                  WHEN l.Letter IN ( 'a', 'S' ) THEN
+                      1
+                  ELSE
+                      0
+             END             AS IsStart,
+             CASE l.Letter
+                  WHEN 'E' THEN
+                      1
+                  ELSE
+                      0
+             END             AS IsEnd
+        FROM letter AS l)
+INSERT dbo.Square (SquareNumber,
+                   Row,
+                   Col,
+                   Elevation,
+                   IsStart,
+                   IsEnd)
+SELECT s.SquareNumber,
+       s.Row,
+       s.Col,
+       s.Elevation,
+       s.IsStart,
+       s.IsEnd
+  FROM square AS s;
+
+INSERT dbo.ValidStep ($from_id,
+                      $to_id)
+SELECT fs.$node_id AS [$from_id],
+       ts.$node_id AS [$to_id]
+  FROM dbo.Square AS fs
+  JOIN dbo.Square AS ts
+    ON ABS(ts.Row - fs.Row) + ABS(ts.Col - fs.Col) = 1
+   AND ts.Elevation                                <= fs.Elevation + 1;
+
+WITH path
+  AS (SELECT       from_square.SquareNumber                                     AS StartSquare,
+                   LAST_VALUE(to_square.SquareNumber) WITHIN GROUP (GRAPH PATH) AS EndSquare,
+                   COUNT(to_square.SquareNumber) WITHIN GROUP (GRAPH PATH)      AS FewestSteps
+        FROM       dbo.Square             AS from_square,
+                   dbo.Square FOR PATH    AS to_square,
+                   dbo.ValidStep FOR PATH AS valid_step
+       WHERE MATCH( SHORTEST_PATH(from_square(-(valid_step)->to_square)+))
+                AND from_square.IsStart = 1)
+SELECT MIN(FewestSteps) AS FewestSteps
+  FROM path
+ WHERE EndSquare = (   SELECT TOP (1)
+                              SquareNumber
+                         FROM dbo.Square
+                        WHERE IsEnd = 1);
